b09358d43dac9f284a348944978960f5e14bbebb
inincevic <inincevic@unipu.hr>
2022-05-21 19:35:57 +0200

    Changed code formatting

--- src/views/Guest.vue
+++ src/views/Guest.vue
@@ -6,16 +6,16 @@
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <form>
-            <div class="form-group">
-              <label for="inputPrimaryType" class="plain_text"></label>
-              <input
-                type="primaryType"
-                class="form-control"
-                id="inputPrimaryType"
-                aria-describedby="emailHelp"
-                placeholder="Enter primary type"
-              />
-            </div>
+          <div class="form-group">
+            <label for="inputPrimaryType" class="plain_text"></label>
+            <input
+              type="primaryType"
+              class="form-control"
+              id="inputPrimaryType"
+              aria-describedby="emailHelp"
+              placeholder="Enter primary type"
+            />
+          </div>
         </form>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -25,23 +25,23 @@
         </p>
       </div>
     </div>
-      
+
     <div class="row">
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <p class="normal_text">Secondary type</p>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <form>
-            <div class="form-group">
-              <label for="inputSecondaryType" class="plain_text"></label>
-              <input
-                type="secondaryType"
-                class="form-control"
-                id="inputSecondaryType"
-                aria-describedby="emailHelp"
-                placeholder="Enter secondary type"
-              />
-            </div>
+          <div class="form-group">
+            <label for="inputSecondaryType" class="plain_text"></label>
+            <input
+              type="secondaryType"
+              class="form-control"
+              id="inputSecondaryType"
+              aria-describedby="emailHelp"
+              placeholder="Enter secondary type"
+            />
+          </div>
         </form>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -51,23 +51,23 @@
         </p>
       </div>
     </div>
-      
+
     <div class="row">
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <p class="normal_text">Primary colour*</p>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <form>
-            <div class="form-group">
-              <label for="inputPrimaryColour" class="plain_text"></label>
-              <input
-                type="primaryColour"
-                class="form-control"
-                id="inputPrimaryColour"
-                aria-describedby="emailHelp"
-                placeholder="Enter primary colour"
-              />
-            </div>
+          <div class="form-group">
+            <label for="inputPrimaryColour" class="plain_text"></label>
+            <input
+              type="primaryColour"
+              class="form-control"
+              id="inputPrimaryColour"
+              aria-describedby="emailHelp"
+              placeholder="Enter primary colour"
+            />
+          </div>
         </form>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -77,23 +77,23 @@
         </p>
       </div>
     </div>
-      
+
     <div class="row">
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <p class="normal_text">Secondary colour</p>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <form>
-            <div class="form-group">
-              <label for="inputSecondaryColour" class="plain_text"></label>
-              <input
-                type="secondaryColour"
-                class="form-control"
-                id="inputSecondaryColour"
-                aria-describedby="emailHelp"
-                placeholder="Enter secondary colour"
-              />
-            </div>
+          <div class="form-group">
+            <label for="inputSecondaryColour" class="plain_text"></label>
+            <input
+              type="secondaryColour"
+              class="form-control"
+              id="inputSecondaryColour"
+              aria-describedby="emailHelp"
+              placeholder="Enter secondary colour"
+            />
+          </div>
         </form>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -103,23 +103,23 @@
         </p>
       </div>
     </div>
-      
+
     <div class="row">
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <p class="normal_text">Form</p>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <form>
-            <div class="form-group">
-              <label for="inputForm" class="plain_text"></label>
-              <input
-                type="form"
-                class="form-control"
-                id="inputForm"
-                aria-describedby="emailHelp"
-                placeholder="Enter form"
-              />
-            </div>
+          <div class="form-group">
+            <label for="inputForm" class="plain_text"></label>
+            <input
+              type="form"
+              class="form-control"
+              id="inputForm"
+              aria-describedby="emailHelp"
+              placeholder="Enter form"
+            />
+          </div>
         </form>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -129,23 +129,23 @@
         </p>
       </div>
     </div>
-      
+
     <div class="row">
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <p class="normal_text">Evolution method</p>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <form>
-            <div class="form-group">
-              <label for="inputEvolutionMethod" class="plain_text"></label>
-              <input
-                type="evolutionMethod"
-                class="form-control"
-                id="inputEvolutionMethod"
-                aria-describedby="emailHelp"
-                placeholder="Enter evolution method"
-              />
-            </div>
+          <div class="form-group">
+            <label for="inputEvolutionMethod" class="plain_text"></label>
+            <input
+              type="evolutionMethod"
+              class="form-control"
+              id="inputEvolutionMethod"
+              aria-describedby="emailHelp"
+              placeholder="Enter evolution method"
+            />
+          </div>
         </form>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -155,23 +155,23 @@
         </p>
       </div>
     </div>
-      
+
     <div class="row">
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <p class="normal_text">Regional variant</p>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <form>
-            <div class="form-group">
-              <label for="inputRegionalVariant" class="plain_text"></label>
-              <input
-                type="regionalVariant"
-                class="form-control"
-                id="inputRegionalVariant"
-                aria-describedby="emailHelp"
-                placeholder="Enter regional variant"
-              />
-            </div>
+          <div class="form-group">
+            <label for="inputRegionalVariant" class="plain_text"></label>
+            <input
+              type="regionalVariant"
+              class="form-control"
+              id="inputRegionalVariant"
+              aria-describedby="emailHelp"
+              placeholder="Enter regional variant"
+            />
+          </div>
         </form>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -188,16 +188,16 @@
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <form>
-            <div class="form-group">
-              <label for="inputDexEntry" class="plain_text"></label>
-              <input
-                type="dexEntry"
-                class="form-control"
-                id="inputDexEntry"
-                aria-describedby="emailHelp"
-                placeholder="Enter dex entry"
-              />
-            </div>
+          <div class="form-group">
+            <label for="inputDexEntry" class="plain_text"></label>
+            <input
+              type="dexEntry"
+              class="form-control"
+              id="inputDexEntry"
+              aria-describedby="emailHelp"
+              placeholder="Enter dex entry"
+            />
+          </div>
         </form>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -214,16 +214,16 @@
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <form>
-            <div class="form-group">
-              <label for="inputBaseStatTotal" class="plain_text"></label>
-              <input
-                type="baseStatTotal"
-                class="form-control"
-                id="inputBaseStatTotal"
-                aria-describedby="emailHelp"
-                placeholder="Enter base stat total"
-              />
-            </div>
+          <div class="form-group">
+            <label for="inputBaseStatTotal" class="plain_text"></label>
+            <input
+              type="baseStatTotal"
+              class="form-control"
+              id="inputBaseStatTotal"
+              aria-describedby="emailHelp"
+              placeholder="Enter base stat total"
+            />
+          </div>
         </form>
       </div>
       <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -244,7 +244,6 @@
       <router-link to="/guest2">Continue</router-link>
       <!-- Note: will have to create this page with display once database and backend communication are complete -->
     </button>
-
   </div>
 </template>
 
