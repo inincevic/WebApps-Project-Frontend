63265431aa75159343401b61923275dbcb09cf7d
inincevic <inincevic@unipu.hr>
2022-06-20 21:59:46 +0200

    Added a page which signifies successful reg

--- src/router/index.js
+++ src/router/index.js
@@ -46,6 +46,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue')
+  },
+  {
+    path: '/RegSuccess',
+    name: 'regsuccess',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/RegSuccess.vue')
   }
 ]
 
---
+++ src/views/RegSuccess.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <br>
+    <br>
+    <h1 class="title_text">Your registration was successful</h1>
+    <br>
+    <p class="plain_text"> Please return to the Home page and log in
+    </p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style scoped>
+@import url("http://fonts.cdnfonts.com/css/pokemon-solid"); /* font-family: 'Pokemon Solid', sans-serif; */
+@import url("http://fonts.cdnfonts.com/css/pokemon-hollow"); /* font-family: 'Pokemon Hollow', sans-serif; */
+@import url(//db.onlinewebfonts.com/c/6120639772c6c60a2fad6742051c6feb?family=Unown);
+
+.title_text {
+  color: #ffcb05;
+  font-family: "Pokemon Solid", sans-serif;
+  font-size: 75px;
+}
+
+.plain_text {
+  color: #ffcb05;
+  font-family: "Unown", sans-serif;
+  font-size: 50px;
+}
+
+
+</style>
